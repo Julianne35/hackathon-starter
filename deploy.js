d7b06c318a8b860313c80a14414f25abf4e933bb
Julianne <jbosko28@gmail.com>
2020-12-05 18:51:02 -0500

    replaced IP address

--- deploy.js
+++ deploy.js
@@ -84,7 +84,7 @@ function sshConnect() {
 
   ssh.connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.87.201.62',
+      host: '54.237.78.74',
       username: 'ubuntu',
       privateKey: 'jd-key.ppk'
     })
